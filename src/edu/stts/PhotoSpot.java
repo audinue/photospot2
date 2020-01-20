4b4c2887692b3c281d6597b274d6350519c3e25e
Audi Nugraha <audinue@gmail.com>
2020-01-20 09:58:06 +0000

    First Commit

---
+++ src/edu/stts/PhotoSpot.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author root
+ */
+public class PhotoSpot extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+    }
+}
